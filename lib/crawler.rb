327ad20dedc7647af9684570cd06ef3fcbb9c1c0
John Richardson <richardsonjm@gmail.com>
2016-02-03 10:20:10 -0500

    Add comments within build map function

--- lib/crawler.rb
+++ lib/crawler.rb
@@ -17,13 +17,16 @@ class Crawler
   end
 
   def build_map(link, dash_count)
+    # print home_page link only on first pass
     print_link(link, dash_count) if link == @home_page
     @browser.visit link
     @visited_links << link
     dash_count += 1
     page_links.each do |link|
+      # only print links within the home_page domain
       next unless link.include? @home_page
       print_link(link, dash_count)
+      # only visit links if they have not been visited before
       next if @visited_links.include? link
       build_map(link, dash_count)
     end
