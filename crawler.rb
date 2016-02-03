8b600fdc0ba96aaddef09e17ded4efdedbae67db
John Richardson <richardsonjm@gmail.com>
2016-02-02 20:33:52 -0500

    Move capybara config into environment file

--- crawler.rb
+++ crawler.rb
@@ -1,15 +1,4 @@
-# Require the gems
-require 'capybara/poltergeist'
-
-# Configure Poltergeist to not blow up on websites with js errors aka every website with js
-# See more options at https://github.com/teampoltergeist/poltergeist#customization
-Capybara.register_driver :poltergeist do |app|
-  Capybara::Poltergeist::Driver.new(app, js_errors: false)
-end
-
-# Configure Capybara to use Poltergeist as the driver
-Capybara.default_driver = :poltergeist
-
+require_relative 'environment.rb'
 
 browser = Capybara.current_session
 # include Capybara::DSL
