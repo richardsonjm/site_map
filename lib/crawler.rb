5d216048a18490d5ee0fefa9b02ad320e97d7662
John Richardson <richardsonjm@gmail.com>
2016-02-03 10:28:35 -0500

    Revert argument order in print_link method

--- lib/crawler.rb
+++ lib/crawler.rb
@@ -7,7 +7,7 @@ class Crawler
     @visited_links = []
   end
 
-  def print_link(link, dash_count)
+  def print_link(dash_count, link)
     dashes = '-'* dash_count
     puts dashes + link
   end
@@ -18,14 +18,14 @@ class Crawler
 
   def build_map(dash_count, link=@home_page)
     # print home_page link only on first pass
-    print_link(link, dash_count) if link == @home_page
+    print_link(dash_count, link) if link == @home_page
     @browser.visit link
     @visited_links << link
     dash_count += 1
     page_links.each do |link|
       # only print links within the home_page domain
       next unless link.include? @home_page
-      print_link(link, dash_count)
+      print_link(dash_count, link)
       # only visit links if they have not been visited before
       next if @visited_links.include? link
       build_map(dash_count, link)
